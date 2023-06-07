1a99853e512be383c904b7b15a25c6e813b81b13
Berat Kuzu <75533278+BeratKuzu55@users.noreply.github.com>
2023-06-07 03:54:00 +0300

    Add files via upload

---
+++ 374InlineStylingReactElements/src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <ul>
+      <li>köfte</li>
+      <li>cola</li>
+      <li>sütlaç</li>
+    </ul>
+    <div>
+      <img src="https://cdn.yemek.com/mnresize/1250/833/uploads/2022/03/ev-koftesi-yemekcom.jpg" />
+      <img src="https://cdn.penceretv.com/news/253796.jpg" />
+      <img src="https://cdn.yemek.com/mnresize/940/940/uploads/2019/04/lokanta-usulu-sutlac-tarifi.jpg" />
+    </div>
+  </div>,
+  document.getElementById("root")
+);
